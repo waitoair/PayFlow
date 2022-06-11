b78eb39e44d2dc92dfc5d544510322ca548ed4f4
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-06-11 00:09:50 -0300

    firebase config

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("HomePage"),
+        title: Text("HomePage"),
       ),
     );
   }
