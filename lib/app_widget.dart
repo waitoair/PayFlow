d039b29904efe51e89cd96e4705b1c3aef713141
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-06-11 00:10:29 -0300

    main dart config with firebase and app_widget

---
+++ lib/app_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'modules/login/login_page.dart';
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+          primarySwatch: Colors.orange,
+          primaryColor: AppColors.primary,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
-import 'modules/login/login_page.dart';
+import 'app_widget.dart';
 
 void main() {
-  runApp(const AppWidget());
+  runApp(AppFirebase());
 }
 
-class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+class AppFirebase extends StatefulWidget {
+  @override
+  State<AppFirebase> createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: const HomePage(),
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Material(
+              child: Center(
+                child: Text(
+                  "Não foi possível inicializar o Firebase",
+                  textDirection: TextDirection.ltr,
+                ),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return AppWidget();
+          } else {
+          return const Material(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      }
     );
   }
 }
